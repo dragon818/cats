8a7d831a582898c571381dcb3a2931ce6f2dddd7
dragon818 <dragon.yao1219@gmail.com>
2020-01-08 10:15:40 -0600

    creat service worker

---
+++ webs.js
@@ -0,0 +1,34 @@
+const cacheName = 'v7';
+const cachedAssets = ['index.html','Contact_Us.html','staff.html','reset.css','styles/about.css','styles/contact.css','styles/home.css','styles/staff.css','main.js'];
+
+self.addEventListener('install', e => {
+  console.log('install');
+  e.waitUntil(
+    caches.open(cacheName).then(cache => { return cache.addAll(cachedAssets);})
+    .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  console.log('avtivate');
+  e.waitUntil(
+    Promise.all([
+      clients.claim(), 
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            if (cache !== cacheName) {
+              return caches.delete(cache);
+            }
+          })
+        )
+        }
+      )
+    ])
+  )
+});
+
+self.addEventListener('fetch', e => {
+  console.log('fetch');
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
